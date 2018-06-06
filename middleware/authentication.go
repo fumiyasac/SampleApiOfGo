f5c35ecad8c4caa62e5012a6322e028c82bcefff
Fumiya Sakai <just1factory@gmail.com>
2018-06-06 23:47:06 +0900

    [WIP] middleware層の作成・検討

---
+++ middleware/authentication.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"os"
+
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+)
+
+// JwtMiddleware check token
+var JwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SIGNINGKEY")), nil
+	},
+	SigningMethod: jwt.SigningMethodHS256,
+})
+
